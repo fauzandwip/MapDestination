2bf135d861dc3a51058e238c57b9548518201bb0
fauzandp <fauzan27dp@gmail.com>
2023-08-23 12:07:17 +0700

    MapDestination App - Swiftful Thinking add assets AppIcon, and images destination assets create Location Model and add LocationDataService data

---
+++ MapDestination/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapDestination
+//
+//  Created by Fauzan Dwi Prasetyo on 23/08/23.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
