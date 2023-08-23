25c927df294d91a8e6e76ec8dd185bf74bd0bf3e
fauzandp <fauzan27dp@gmail.com>
2023-08-23 11:37:28 +0700

    Initial Commit

---
+++ MapDestination/MapDestinationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapDestinationApp.swift
+//  MapDestination
+//
+//  Created by Fauzan Dwi Prasetyo on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MapDestinationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
